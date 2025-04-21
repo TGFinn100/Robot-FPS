bb390afb35dc000f5a33a9f9aaaf5e7c12dc95d3
TGFinn100 <dalyfinnegan6@gmail.com>
2025-04-21 14:05:57 +0100

    another

--- FPS NetCode/Assets/Scripts/Managers/ui/MainMenuManager.cs
+++ FPS NetCode/Assets/Scripts/Managers/ui/MainMenuManager.cs
@@ -4,7 +4,7 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenuManager : MonoBehaviour
+public class MainMenuManager : NetworkBehaviour
 {
 	public string gameScene;
 
@@ -18,7 +18,19 @@ public class MainMenuManager : MonoBehaviour
 
 		Debug.Log($"Starting game, changing scene ({this.name})");
 		//SceneManager.LoadScene(gameScene);
-		NetworkManager.Singleton.SceneManager.LoadScene(gameScene, LoadSceneMode.Single);
+		//NetworkManager.Singleton.SceneManager.LoadScene(gameScene, LoadSceneMode.Single);
+
+
+
+		if (IsServer && !string.IsNullOrEmpty(gameScene))
+		{
+			var status = NetworkManager.SceneManager.LoadScene(gameScene, LoadSceneMode.Single);
+			if (status != SceneEventProgressStatus.Started)
+			{
+				Debug.LogWarning($"Failed to load {gameScene} " +
+					  $"with a {nameof(SceneEventProgressStatus)}: {status} ({this.name})");
+			}
+		}
 	}
 
 	public void LoadOut()
